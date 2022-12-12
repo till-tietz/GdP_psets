c15fa58e8b2903a218c2020017050ae583cc7e18
till-tietz <ttietz2014@gmail.com>
2022-12-12 02:00:04 +0100

    pset 6 completed

--- pset_6/test.java
+++ pset_6/test.java
@@ -1,5 +1,4 @@
-public class Bigs {
-
+public class test {
     //helper function for add 
     //let x be the greater of the two numbers 
     static int[] getSum (int[] x, int[] y){
@@ -48,56 +47,23 @@ public class Bigs {
         }
     }
 
-    // gibt das Ziffernfeld n in lesbarer dezimaler Form aus
-    // bei sehr langen Zahlen soll das Format verwendet werden, welches auch von
-    // benutzt wird: Umbruch nach 68 Ziffern mit einem \ am Ende
-    static void print (int[] n) {
-        String ret = "";
-        for(int i = n.length - 1; i >= 0; i--){
-            ret = ret + n[i];
-            if((n.length - i) % 68 == 0){
-                ret = ret + "\n";
-            }
+    static int[] times(int[] n, int d) {
+        int[] ret = n;
+        for(int i = 1; i < d; i++){
+            ret = add(ret,n);
         }
-        if(n.length > 68){
-            ret = ret + "\\";
-        }
-        System.out.println(ret);
-    }
-
-    // konstruiert ein einstelliges Ziffernfeld aus der Ziffer d
-    static int[] digit(int d) {
-        int[] ret = {d};
         return ret;
     }
 
-    // konstruiert das Ziffernfeld, welches den Wert Null repraesentiert
-    static int[] Null() {
-        int[] ret = {0};
-        return ret;
-    }
-
-    // konstruiert das Ziffernfeld, welches den Wert Eins repraesentiert
-    static int[] One() {
-        int[] ret = {1};
-        return ret;
-    }
-
-    // Rest des Ziffernfeldes n bei Division durch 10 (eine int-Zahl!)
-    static int mod10(int[] n) {
-        return n[0];
-    }
-
-    // ganzzahliger Quotient bei Division durch 10
-    static int[] div10(int[] n) {
-        int[] ret = new int[n.length - 1];
+    static int[] times10(int[] n) {
+        int[] ret = new int[n.length + 1];
+        ret[0] = 0;
         for(int i = 1; i < ret.length; i++){
-            ret[i] = n[i];
+            ret[i] = n[i-1];
         }
         return ret;
     }
 
-    // Umwandlung einer beliebigen int-Zahl in ein Ziffernfeld
     static int[] fromInt(int n) {
         int digits = String.valueOf(n).length();
         int[] ret = new int[digits];
@@ -110,32 +76,18 @@ public class Bigs {
         return ret;
     }
 
-    // kopiert den Wert von n
-    static int[] copy(int[] n) {
-        int[] ret = new int[n.length];
-        for(int i = 0; i < n.length; i++){
-            ret[i] = n[i];
+    static void print (int[] n) {
+        String ret = "";
+        for(int i = n.length - 1; i >= 0; i--){
+            ret = ret + n[i];
+            if((n.length - i) % 68 == 0){
+                ret = ret + "\n";
+            }
         }
-        return ret;
-    }
-
-    // multipliziert das Ziffernfeld n mit einer (einstelligen!) int-Zahl
-    static int[] times(int[] n, int d) {
-        int[] ret = n;
-        for(int i = 1; i < d; i++){
-            ret = add(ret,n);
+        if(n.length > 68){
+            ret = ret + "\\";
         }
-        return ret;
-    }
-
-    // multipliziert das Ziffernfeld n mit 10
-    static int[] times10(int[] n) {
-        int[] ret = new int[n.length + 1];
-        ret[0] = 0;
-        for(int i = 1; i < ret.length; i++){
-            ret[i] = n[i-1];
-        }
-        return ret;
+        System.out.println(ret);
     }
 
     //helper function for times
@@ -154,8 +106,14 @@ public class Bigs {
         return prod;
     } 
 
-    // multipliziert zwei Ziffernfelder
-    static int[] times(int[] a, int[] b) {
+    // konstruiert das Ziffernfeld, welches den Wert Null repraesentiert
+    static int[] Null() {
+        int[] ret = {0};
+        return ret;
+    }
+
+     // multipliziert zwei Ziffernfelder
+     static int[] times(int[] a, int[] b) {
         if (a.length >= b.length){
             return getProd(a,b);
         } else {
@@ -163,7 +121,6 @@ public class Bigs {
         }
     }
 
-    // Quadratzahl eines Ziffernfeldes
     static int[] square(int[] a) {
         return times(a,a);
     }
@@ -174,7 +131,6 @@ public class Bigs {
         return times(ret,a);
     }
 
-    // Test auf kleiner-Relation zweier Ziffernfelder: a < b ?
     static boolean less (int[] a, int[] b) {
         int aLength = a.length;
         int bLength = b.length;
@@ -214,9 +170,6 @@ public class Bigs {
         return isEqual;  
     }
 
-    // Test auf Korrektheit eines Ziffernfeldes: Feld existiert und enthaelt
-    // mindenstens eine Ziffer, alle Positionen liegen zwischen 0 und 9
-    // keine fuehrenden Nullen (ausser bei Null selbst)
     static boolean ok (int[] n) {
         int nLength = n.length;
         if(nLength == 0){
@@ -256,24 +209,17 @@ public class Bigs {
         }
         return;
     }
-    
-    public static void main (String[] s) {
-        int[] a = One();
-        for (int i=0; i<33222; ++i) { a = times(a, 2); }
-        System.out.println("2^33222 hat " + a.length + " Stellen");
-        print(a);
-        System.out.println();
-        int[ ] b = fromInt(13);
-        int[ ] c = copy(b);
-        for (int i=1; i<8978; ++i) { c = times(c, b); }
-        System.out.println("13^8978 hat " + c.length + " Stellen");
-        print(c);
-        System.out.println();
-        System.out.println(less(a, c)); // beantwortet die Frage aus der
-        // Aufgabenueberschrift
-        maxDigit(a);
-        maxDigit(c);
+
+    public static void main (String[] args) {
+        int[] x = {5,7};
+        int[] y = {5,7};
+        int[] z = {5,1,7,2,2,2,3};
+
+        int[] ret = cubic(x);
+        print(ret);
+
+        System.out.println(equal(y,x));
+        System.out.println(ok(z));
+        maxDigit(z);
     }
-
-
 }
